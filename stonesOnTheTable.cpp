1feb736458623396f186dc2a49c99dfcff63707c
IvanTicona <ivan.ticona.v@gmail.com>
2024-04-15 12:02:14 -0400

    Accepted

--- stonesOnTheTable.cpp
+++ stonesOnTheTable.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 int main(){
 
-
+  int n,c=0;string s;
+  cin >> n >> s;
+  for (int i = 0; i < s.size(); i++){
+    if(s[i] == s[i+1]){
+      s.erase(i,1);
+      i--;
+      c++;
+    }
+  }
+  cout<<c;
 
   return 0;
 }
