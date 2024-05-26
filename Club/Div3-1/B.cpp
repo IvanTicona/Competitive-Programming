c32c993a737ad9b3213906e8bc2c215f0333ceea
IvanTicona <ivan.ticona.v@gmail.com>
2024-05-25 23:15:03 -0400

    Accepted

--- Club/Div3-1/B.cpp
+++ Club/Div3-1/B.cpp
@@ -3,16 +3,24 @@
 using namespace std;
 
 void sumParity(int n, int k) {
-  if (n < k || (n%2 == 1 && k%2 == 0)) {
+  if (n < k || n%2 == 1 && k%2 == 0) {
       cout<<"NO"<<endl;
       return;
   }
-  if (n >= k && (n % 2 == k % 2) || n >= 2 * k) {
+  if (n >= k && (n % 2 == k % 2)) {
       cout<<"YES"<<endl;
       for (int i = 0; i < k - 1; ++i) {
           cout<<1<<" ";
       }
       cout<<(n - (k - 1))<<endl;
+      return;
+  }
+  if (n >= 2 * k) {
+      cout<<"YES"<<endl;
+      for (int i = 0; i < k - 1; ++i) {
+          cout<<2<<" ";
+      }
+      cout<<(n - 2 * (k - 1))<<endl;
       return;
   }
   cout<<"NO"<<endl;
