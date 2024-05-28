87efa5a936d36a16a5fa07816b760ec1faf6c709
IvanTicona <ivan.ticona.v@gmail.com>
2024-05-27 21:26:04 -0400

    Accepted

--- Club/Div3-1/C.cpp
+++ Club/Div3-1/C.cpp
@@ -1,10 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+#define input freopen("input.txt", "r", stdin)
+
 int main(){
-
-  
-
-  return 0;
+  input;
+  int t, count;
+  cin >> t;
+  while (t--){
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i = i * 10){
+      if ((n / i) % 10 != 0) count++;
+    }
+    cout << count << endl;
+    count = 0;
+    for (int i = 10; i <= n * 10; i = i * 10){
+      if (n % i != 0) cout << n % i << " ";
+      n -= n % i;
+    }
+    cout << endl;
+  }
 }
