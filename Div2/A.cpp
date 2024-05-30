ecc3e612872298ba18194d72df2d8824e3b6954b
IvanTicona <ivan.ticona.v@gmail.com>
2024-05-30 13:44:41 -0400

    Accepted

--- Div2/A.cpp
+++ Div2/A.cpp
@@ -10,6 +10,10 @@ int main(){
   while (t--){
     cin >> n >> pass;
     bool invalid = true;
+    if(pass.length() <= 1){
+      cout << "YES" << endl;
+      continue;
+    }
     for (int i = 0; i < n-1; i++){
       if((pass[i]>= 'a' && pass[i] <= 'z')||(pass[i] >= '0' && pass[i] <= '9')){
         if(pass[i] <= pass[i+1]){
@@ -27,50 +31,3 @@ int main(){
 
   return 0;
 }
-
-// int main()
-// {
-//   int t, n;
-//   string pass;
-//   cin >> t;
-//   while (t--)
-//   {
-//     cin >> n >> pass;
-//     bool invalid = false;
-
-//     for (int i = 0; i < n - 1; ++i)
-//     {
-//       if (isalpha(pass[i]) && isdigit(pass[i + 1]))
-//       {
-//         invalid = true;
-//         break;
-//       }
-//       if (pass[i] > pass[i + 1])
-//       {
-//         invalid = true;
-//         break;
-//       }
-//     }
-//     if (isdigit(pass[n - 1]))
-//     {
-//       for (int i = 0; i < n; ++i)
-//       {
-//         if (isalpha(pass[i]))
-//         {
-//           invalid = true;
-//           break;
-//         }
-//       }
-//     }
-//     if (!invalid)
-//     {
-//       cout << "YES" << endl;
-//     }
-//     else
-//     {
-//       cout << "NO" << endl;
-//     }
-//   }
-
-//   return 0;
-// }
