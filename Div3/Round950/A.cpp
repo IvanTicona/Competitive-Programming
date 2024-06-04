254810d8225a50248082488e765c28132cf4fa43
IvanTicona <ivan.ticona.v@gmail.com>
2024-06-03 22:31:58 -0400

    Accepted

--- Div3/Round950/A.cpp
+++ Div3/Round950/A.cpp
@@ -4,6 +4,27 @@ using namespace std;
 
 int main(){
 
+  int t;
+  cin>>t;
+  while (t--){
+    int n,m;
+    cin>>n>>m;
+    int need = 7*n;
+    string a;
+    cin>>a;
+    int arr[7];
+    fill(arr, arr+7, m);
+    for(int i = 0; i < n; i++){
+      if(arr[a[i]-65]>0) arr[a[i]-65]--;
+    }
+    int count=0;
+    for (int i = 0; i < 7; i++){
+      count+=arr[i];
+    }
+    cout<<count<<endl;
+  }
+  
+
   
 
   return 0;
