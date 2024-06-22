b56395cc226d8bdee94cb186191f464cea811fcb
Ivan Ticona <ivan.ticona.v@gmail.com>
2024-06-22 16:49:12 -0400

    Accepted

--- Club/Div4-1/E.cpp
+++ Club/Div4-1/E.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 int main(){
 
-  
+  int mat[10][10];
+  for(int i=0;i<10;i++){mat[i][0]=1;mat[0][i]=1;}
+  for(int i=1;i<10;i++){
+    for(int j=1;j<10;j++){
+      mat[i][j]=mat[i-1][j]+mat[i][j-1];
+    }
+  }
+  int n;
+  cin>>n;
+  cout<<mat[n-1][n-1]<<endl;
 
   return 0;
 }
