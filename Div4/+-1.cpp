a11829edf6985ab9eee4ae23c8314d1295a91141
Ivan Ticona <ivan.ticona.v@gmail.com>
2024-06-22 02:42:23 -0400

    Accepted

---
+++ Div4/+-1.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+  int t;
+  cin>>t;
+  while (t--){
+    int n;
+    int x[n];
+    int y[n];
+    int z[n];
+    for (int i = 0; i < n; i++)
+    {
+      cin>>x[i]>>y[i]>>z[i];
+    }
+    // 1 -1 -1 1
+  }
+
+  return 0;
+}
