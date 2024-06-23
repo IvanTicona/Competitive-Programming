a935e30af3f1d8cf2706d74a2d7dd2fba9596679
Ivan Ticona <ivan.ticona.v@gmail.com>
2024-06-23 03:10:31 -0400

    Accepted

--- Club/Div3-1/B.cpp
+++ Club/Div3-1/B.cpp
@@ -2,37 +2,22 @@
  
 using namespace std;
  
-void sumParity(int n, int k) {
-  if (n < k || (n%2 == 1 && k%2 == 0)) {
-      cout<<"NO"<<endl;
-      return;
-  }
-  if (n >= k && (n % 2 == k % 2)) {
-      cout<<"YES"<<endl;
-      for (int i = 0; i < k - 1; ++i) {
-          cout<<1<<" ";
-      }
-      cout<<(n - (k - 1))<<endl;
-      return;
-  }
-  if (n >= 2 * k) {
-      cout<<"YES"<<endl;
-      for (int i = 0; i < k - 1; ++i) {
-          cout<<2<<" ";
-      }
-      cout<<(n - 2 * (k - 1))<<endl;
-      return;
-  }
-  cout<<"NO"<<endl;
-}
- 
 int main() {
   int t;
   cin >> t;
   while(t--){
     int n, k;
     cin>>n>>k;
-    sumParity(n, k);
+    if((n-(k-1))%2==1 && n-(k-1)>0){
+      cout<<"YES"<<endl;
+      for(int i=0; i<k-1; i++) cout<<"1 ";
+      cout<<n-(k-1)<<endl;
+    }else if((n-(2*(k-1)))%2==0 && n-(2*(k-1))>0){
+      cout<<"YES"<<endl;
+      for(int i=0; i<k-1; i++) cout<<"2 ";
+      cout<<n-(2*(k-1))<<endl;
+    }else cout<<"NO"<<endl;
+
   }
   return 0;
 }
