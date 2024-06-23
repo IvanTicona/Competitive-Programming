944f54f1fc0d23988a4067c5974272004319b95d
Ivan Ticona <ivan.ticona.v@gmail.com>
2024-06-23 03:26:39 -0400

    Accepted

--- Club/Div3-1/C.cpp
+++ Club/Div3-1/C.cpp
@@ -6,22 +6,10 @@ int main(){
 
   int t, c;
   cin>>t;
-  while (t--)
-  {
+  while (t--){
     int n;
     cin>>n;
-    
-    for (int i = 1; i <= n; i = i * 10){
-      if ((n / i) % 10 != 0) c++;
-    }
-    cout<<c<<endl;
-    c = 0;
-    for (int i = 10; i <= n * 10; i = i * 10){
-      if (n % i != 0) cout<<n % i<<" ";
-      n -= n % i;
-    }
-    cout<<endl;
+    //Descomponer el numeros redondos
   }
-
   return 0;
 }
