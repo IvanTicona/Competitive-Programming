a748bd5242b3daf0a90eb4d8ecb2694cb1957d92
Ivan Ticona <ivan.ticona.v@gmail.com>
2024-07-09 18:38:50 -0400

    Accepted

--- Club/Grafos/10kindsOfPeople.cpp
+++ Club/Grafos/10kindsOfPeople.cpp
@@ -3,21 +3,21 @@
 using namespace std;
 
 vector<string> mapita;
-bool vis[1010][1010];
+int area[1010][1010];
 
 int lf, lc;
 
 int f[]={0,0,1,-1};
 int c[]={1,-1,0,0};
 
-void dfs(int fa, int ca, char bin){
-  vis[fa][ca]=true;
+void dfs(int fa, int ca, int ar, char bin){
+  if(area[fa][ca]!=0) return;
+  area[fa][ca]=ar;
   for (int i = 0; i < 4; i++){
     int nf=fa+f[i];
     int nc=ca+c[i];
-    if(nf<0 || nc<0 || nf>=lf || nc>=lc) continue;
-    if(vis[nf][nc]||mapita[nf][nc]!=bin) continue;
-    dfs(nf,nc,bin);
+    if(nf<0 || nc<0 || nf>=lf || nc>=lc || mapita[nf][nc]!=bin) continue;
+    dfs(nf,nc,ar,bin);
   }
 }
 
@@ -31,31 +31,26 @@ int main(){
     cin>>l;
     mapita.push_back(l);
   }
+  int ar=1;
+  memset(area,0,sizeof(area));
+  for (int i = 0; i < r; i++){
+    for (int j = 0; j < c; j++){
+      if(area[i][j]==0){
+        dfs(i,j,ar,mapita[i][j]);
+        ar++;
+      }
+    }
+  }
   int n;
   cin>>n;
   while (n--){
     int r1,r2,c1,c2;
     cin>>r1>>c1>>r2>>c2;
-
-    if(mapita[r1-1][c1-1]=='1'){
-      memset(vis,false,sizeof(vis));
-      dfs(r1-1,c1-1,'1');
-      if(vis[r2-1][c2-1]){
-        cout<<"decimal"<<endl;
-        continue;
-      }
-    }else if(mapita[r1-1][c1-1]=='0'){
-      memset(vis,false,sizeof(vis));
-      dfs(r1-1,c1-1,'0');
-      if(vis[r2-1][r2-1]){
-        cout<<"binary"<<endl;
-        continue;
-      }
-    }
-    memset(vis,false,sizeof(vis));
-    cout<<"neither"<<endl;
+    r1--,c1--,r2--,c2--;
+    if(mapita[r1][c1]=='0' && area[r1][c1]==area[r2][c2]) cout<<"binary"<<endl;
+    else if(mapita[r1][c1]=='1' && area[r1][c1]==area[r2][c2]) cout<<"decimal"<<endl;
+    else cout<<"neither"<<endl;
   }
-  //Tratarlos como areas, y si los puntos estan en la misma area o es binary o es decimal sino neither
 
   return 0;
 }
