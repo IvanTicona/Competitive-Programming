d67a46199a1904d42f80fd7b7f6b2a57f7a4e4e4
Ivan Ticona <ivan.ticona.v@gmail.com>
2024-07-13 00:01:05 -0400

    Accepted

--- Club/Grafos/brexit.cpp
+++ Club/Grafos/brexit.cpp
@@ -1,49 +1,48 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 vector<int> countries[200010];
-int degree[200010];
-int leftPartners[200010];
-bool hasLeft[200010];
+int partner[200010];
+int leavepartner[200010];
+bool leave[200010];
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    int C, P, X, L;
-    cin >> C >> P >> X >> L;
-    
-    for (int i = 0; i < P; i++) {
-        int A, B;
-        cin >> A >> B;
-        countries[A].push_back(B);
-        countries[B].push_back(A);
-        degree[A]++;
-        degree[B]++;
+int main(){
+
+  int C, P, X, L;
+  cin>>C>>P>>X>>L;
+
+  for (int i = 0; i < P; i++){
+    int A, B;
+    cin>>A>>B;
+    countries[A].push_back(B);
+    countries[B].push_back(A);
+    partner[A]++;
+    partner[B]++;
+  }
+
+  queue<int> pq;
+  pq.push(L);
+  leave[L]=true;
+
+  while (!pq.empty()){
+    int na=pq.front();
+    pq.pop();
+
+    for (auto &vec: countries[na]){
+      leavepartner[vec]++;
+      if (!leave[vec] && leavepartner[vec]*2 >= partner[vec]){
+        leave[vec]=true;
+        pq.push(vec);
+      }
     }
+  }
 
-    queue<int> q;
-    q.push(L);
-    hasLeft[L] = true;
+  if (leave[X]){
+    cout<<"leave"<<endl;
+  }else{
+    cout<<"stay"<<endl;
+  }
 
-    while (!q.empty()) {
-        int na = q.front();
-        q.pop();
-
-        for (auto &neighbor : countries[na]) {
-            leftPartners[neighbor]++;
-            if (!hasLeft[neighbor] && leftPartners[neighbor] * 2 >= degree[neighbor]) {
-                hasLeft[neighbor] = true;
-                q.push(neighbor);
-            }
-        }
-    }
-
-    if (hasLeft[X]) {
-        cout << "leave" << endl;
-    } else {
-        cout << "stay" << endl;
-    }
-
-    return 0;
+  return 0;
 }
