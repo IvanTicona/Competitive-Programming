b02de35535c815cb821ca03a3db555b722a29698
Ivan Ticona <ivan.ticona.v@gmail.com>
2024-07-29 18:18:00 -0400

    Accepted

--- Club/Basic DP/chocolates.cpp
+++ Club/Basic DP/chocolates.cpp
@@ -2,126 +2,110 @@
 
 using namespace std;
 
-const int MAX_R = 4;
-const int MAX_C = 4;
+// const int MAX_R = 4;
+// const int MAX_C = 4;
+// const int dc4[4] = {1, 0, -1, 0};
+// const int dr4[4] = {0, 1, 0, -1};
+// const int dr8[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+// const int dc8[8] = {1, 0, -1, 0, 1, -1, 1, -1};
 
-// Direcciones para moverse en el tablero
-int dr4[4] = {0, 1, 0, -1};
-int dc4[4] = {1, 0, -1, 0};
-int dr8[8] = {0, 1, 0, -1, 1, 1, -1, -1};
-int dc8[8] = {1, 0, -1, 0, 1, -1, 1, -1};
+// bool isValid(int x, int y, int R, int C) {
+//   return x >= 0 && x < R && y >= 0 && y < C;
+// }
 
-// Verifica si las coordenadas están dentro de los límites del tablero
-bool isValid(int x, int y, int R, int C) {
-    return x >= 0 && x < R && y >= 0 && y < C;
-}
+// bool checkPolygon(int R, int C, int p) {
+//   if (p == 0) return false;
 
-bool checkPolygon(int R, int C, int p) {
-    if (p == 0) return false; // no es un polígono si no hay chocolates
+//   vector<vector<int>> board(R, vector<int>(C, 0));
+//   pair<int, int> source = {-1, -1};
 
-    vector<vector<int>> board(R, vector<int>(C, 0));
-    pair<int, int> source = {-1, -1};
+//   for (int i = 0; i < R * C; ++i) {
+//     if (p & (1 << i)) {
+//       int r = i / C, c = i % C;
+//       board[r][c] = 1;
+//       if (source == make_pair(-1, -1)) {
+//         source = {r, c};
+//       }
+//     }
+//   }
 
-    // Llenar el tablero según la máscara p
-    for (int r = 0; r < R; ++r) {
-        for (int c = 0; c < C; ++c) {
-            if ((1 << (r * C + c)) & p) {
-                board[r][c] = 1;
-                if (source == make_pair(-1, -1)) {
-                    source = {r, c};
-                }
-            }
-        }
-    }
+//   queue<pair<int, int>> q;
+//   vector<vector<bool>> visited(R, vector<bool>(C, false));
+//   q.push(source);
+//   visited[source.first][source.second] = true;
 
-    // Verificar si todos los chocolates están en un solo componente conectado
-    queue<pair<int, int>> q;
-    vector<vector<bool>> visited(R, vector<bool>(C, false));
-    q.push(source);
-    visited[source.first][source.second] = true;
+//   while (!q.empty()) {
+//     auto [r, c] = q.front(); q.pop();
+//     for (int i = 0; i < 4; ++i) {
+//       int nr = r + dr4[i], nc = c + dc4[i];
+//       if (isValid(nr, nc, R, C) && !visited[nr][nc] && board[nr][nc]) {
+//         q.push({nr, nc});
+//         visited[nr][nc] = true;
+//       }
+//     }
+//   }
 
-    while (!q.empty()) {
-        auto [r, c] = q.front();
-        q.pop();
-        for (int i = 0; i < 4; ++i) {
-            int nr = r + dr4[i];
-            int nc = c + dc4[i];
-            if (isValid(nr, nc, R, C) && !visited[nr][nc] && board[nr][nc] == 1) {
-                q.push({nr, nc});
-                visited[nr][nc] = true;
-            }
-        }
-    }
+//   for (int r = 0; r < R; ++r)
+//       for (int c = 0; c < C; ++c)
+//           if (board[r][c] && !visited[r][c])
+//               return false;
 
-    for (int r = 0; r < R; ++r) {
-        for (int c = 0; c < C; ++c) {
-            if (board[r][c] == 1 && !visited[r][c]) {
-                return false;
-            }
-        }
-    }
+//   q = queue<pair<int, int>>();
+//   visited = vector<vector<bool>>(R, vector<bool>(C, false));
 
-    // Verificar que todas las celdas vacías estén conectadas a los bordes
-    q = queue<pair<int, int>>();
-    visited = vector<vector<bool>>(R, vector<bool>(C, false));
+//   for (int r = 0; r < R; ++r) {
+//     if (!board[r][0]) q.push({r, 0});
+//     if (!board[r][C - 1]) q.push({r, C - 1});
+//   }
+//   for (int c = 1; c < C - 1; ++c) {
+//     if (!board[0][c]) q.push({0, c});
+//     if (!board[R - 1][c]) q.push({R - 1, c});
+//   }
 
-    for (int r = 0; r < R; ++r) {
-        if (board[r][0] == 0) q.push({r, 0});
-        if (board[r][C-1] == 0) q.push({r, C-1});
-    }
-    for (int c = 1; c < C-1; ++c) {
-        if (board[0][c] == 0) q.push({0, c});
-        if (board[R-1][c] == 0) q.push({R-1, c});
-    }
+//   while (!q.empty()) {
+//     auto [r, c] = q.front(); q.pop();
+//     visited[r][c] = true;
+//     for (int i = 0; i < 8; ++i) {
+//       int nr = r + dr8[i], nc = c + dc8[i];
+//       if (isValid(nr, nc, R, C) && !visited[nr][nc] && !board[nr][nc]) {
+//         q.push({nr, nc});
+//         visited[nr][nc] = true;
+//       }
+//     }
+//   }
 
-    while (!q.empty()) {
-        auto [r, c] = q.front();
-        q.pop();
-        visited[r][c] = true;
-        for (int i = 0; i < 8; ++i) {
-            int nr = r + dr8[i];
-            int nc = c + dc8[i];
-            if (isValid(nr, nc, R, C) && !visited[nr][nc] && board[nr][nc] == 0) {
-                q.push({nr, nc});
-                visited[nr][nc] = true;
-            }
-        }
-    }
+//   for (int r = 0; r < R; ++r)
+//     for (int c = 0; c < C; ++c)
+//       if (!board[r][c] && !visited[r][c])
+//         return false;
 
-    for (int r = 0; r < R; ++r) {
-        for (int c = 0; c < C; ++c) {
-            if (board[r][c] == 0 && !visited[r][c]) {
-                return false;
-            }
-        }
-    }
-
-    return true;
-}
+//   return true;
+// }
 
 int main() {
-    int answer_matrix[MAX_R][MAX_C] = {0};
+  // int answer_matrix[MAX_R][MAX_C] = {0};
 
-    for (int h = 1; h <= MAX_R; ++h) {
-        for (int w = 1; w <= MAX_C; ++w) {
-            for (int p = 1; p < (1 << (h * w)); ++p) {
-                if (checkPolygon(h, w, p)) {
-                    answer_matrix[h-1][w-1]++;
-                }
-            }
-        }
-    }
+  // for (int h = 1; h <= MAX_R; ++h) {
+  //   for (int w = 1; w <= MAX_C; ++w) {
+  //     for (int p = 1; p < (1 << (h * w)); ++p) {
+  //       if (checkPolygon(h, w, p)) {
+  //         answer_matrix[h - 1][w - 1]++;
+  //       }
+  //     }
+  //   }
+  // }
 
-    for (int h = 1; h <= MAX_R; ++h) {
-        for (int w = 1; w <= MAX_C; ++w) {
-            cout << "Polygons for " << h << "x" << w << ": " << answer_matrix[h-1][w-1] << endl;
-        }
-    }
+  int results[4][4] = {
+    {1, 3, 6, 10},
+    {3, 13, 40, 108},
+    {6, 40, 217, 1109},
+    {10, 108, 1109, 10977}
+  };
 
-    // Lectura de la entrada
-    int h, w;
-    cin >> h >> w;
-    cout << answer_matrix[h-1][w-1] << endl;
+  int h, w;
+  cin >> h >> w;
+  // cout << answer_matrix[h - 1][w - 1] << endl;
+  cout << results[h - 1][w - 1] << endl;
 
-    return 0;
+  return 0;
 }
