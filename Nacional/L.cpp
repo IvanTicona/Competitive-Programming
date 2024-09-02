cb874a2ed4b2ccebad16036dfbc360c23aaa6049
Ivan Ticona <ivan.ticona.v@gmail.com>
2024-09-01 20:38:25 -0400

    Updated

--- Nacional/L.cpp
+++ Nacional/L.cpp
@@ -3,27 +3,24 @@
 using namespace std;
 typedef vector<int> vi;
 
-vi transformBits(vi &nums){
-  
+vi lexicography(vi &nums){
+
   vi result(nums.size(), 0);
 
-  // Iteramos por cada bit (asumiendo que trabajamos con enteros de 32 bits)
-  for (int bit = 0; bit < 32; ++bit){
-    int bitCount = 0;
+  for (int bit = 0; bit < 32; bit++){
 
-    // Contamos los bits en la posición actual para todos los números
-    for (int num : nums){
+    int count = 0, i = 0;
+
+    for (int num: nums){
       if (num & (1 << bit)){
-        ++bitCount;
+        count++;
       }
     }
 
-    // Colocamos el bit en el resultado correspondiente a la posición del bit
-    for (int i = 0; i < nums.size(); ++i){
-      if (bitCount > 0){
-        result[i] |= (1 << bit);
-        --bitCount;
-      }
+    while (count>0){
+      result[i] |= (1 << bit);
+      count--;
+      i++;
     }
   }
 
@@ -36,8 +33,8 @@ int main(){
   vi nums(N);
   for (int &v: nums) cin>>v;
 
-  vi result = transformBits(nums);
-  for (int num : result) cout<<num<<" ";
+  vi result = lexicography(nums);
+  for (int num: result) cout<<num<<" ";
 
   return 0;
 }
