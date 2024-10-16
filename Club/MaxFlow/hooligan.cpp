b2d8b9506cf4471eea929d183e65b883fd87ba15
Dorian Ivan Ticona Vega <66503891+IvanTicona@users.noreply.github.com>
2024-10-15 23:05:27 -0400

    Updated

---
+++ Club/MaxFlow/hooligan.cpp
@@ -0,0 +1,26 @@
+/*
+Author: Ivan Ticona
+*/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+
+  int N, M, G;
+
+  while (cin >> N >> M >> G && N && M && G)
+  {
+    for (int i = 0; i < G; i++)
+    {
+      int a, b;
+    }
+    
+  }
+  
+
+  return 0;
+}
