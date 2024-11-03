7226547ff6b7f07620c321c5433fd3cddbb0bffa
Dorian Ivan Ticona Vega <66503891+IvanTicona@users.noreply.github.com>
2024-11-02 23:06:30 -0400

    Updated

--- Contest/2022-2023ICPC/C.cpp
+++ Contest/2022-2023ICPC/C.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 
-#define fore(i, a, b) for (int i = a, ThxDem = b; i < ThxDem; ++i)
-
 using namespace std;
+
+#define fore(i,a,b) for(int i=a,ThxDem=b;i<ThxDem;++i)
 
 const int MAXN = 1010;
 bool dp[MAXN][MAXN];
 
-int main()
-{
+int main(){
+
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
@@ -21,22 +21,24 @@ int main()
   int l = e, r = n - k - e;
 
   dp[0][0] = 1;
-  fore(len, 1, n + 1) if (len != k)
-  {
-    for (int i = l; i >= 0; i--)
-      for (int j = r; j >= 0; j--)
-      {
-        if (len <= i)
-          dp[i][j] |= dp[i - len][j];
-        if (len <= j)
-          dp[i][j] |= dp[i][j - len];
+  for(int len = 1; len < n + 1; len++){
+    if (len != k){
+      for (int i = l; i >= 0; i--){
+        for (int j = r; j >= 0; j--){
+          if (len <= i) dp[i][j] |= dp[i - len][j];
+          if (len <= j) dp[i][j] |= dp[i][j - len];
+        }
       }
+    }
   }
 
   int ans = k;
-  fore(i, 0, l + 1) fore(j, 0, r + 1) if (dp[i][j])
-  {
-    ans = max(ans, i + j + k);
+  for(int i = 0; i < l + 1; i++){
+    for(int j = 0; j < r + 1; j++){
+      if (dp[i][j]){
+        ans = max(ans, i + j + k);
+      }
+    }
   }
 
   cout << n - ans << "\n";
