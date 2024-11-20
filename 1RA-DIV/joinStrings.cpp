4010489b16d77359a757dc2a5442a10c42526185
Dorian Ivan Ticona Vega <66503891+IvanTicona@users.noreply.github.com>
2024-11-19 21:13:20 -0400

    Accepted

--- 1RA-DIV/joinStrings.cpp
+++ 1RA-DIV/joinStrings.cpp
@@ -2,39 +2,40 @@
 
 using namespace std;
 
-void dfs(vector<vector<int>> &v, vector<string> &s, int idx) {
-  cout<<s[idx];
-  for(int i: v[idx]) {
-    dfs(v, s, i);
+typedef vector<vector<int>> vvi;
+typedef vector<string> vs;
+
+void dfs(vvi &grafo, vs &s, int idx) {
+  cout << s[idx];
+  for(int i: grafo[idx]) {
+    dfs(grafo, s, i);
   }
 }
 
 int main() {
 
-  //FAST I/O
-  ios_base::sync_with_stdio(false);
+  ios_base:: sync_with_stdio(false);
   cin.tie(NULL); cout.tie(NULL);
   
-  int n;
-  cin>>n;
-  vector<string> words(n);
+  int n; cin >> n;
+  vs words(n);
   for(int i = 0; i < n; i++) {
-    cin>>words[i];
+    cin >> words[i];
   }
 
   int idx1, idx2, lastIdx = 0;
-  vector<vector<int>> v(n);
+  vvi grafo(n);
 
   for(int i = 0; i < n-1; i++) {
-    cin>>idx1>>idx2;
+    cin >> idx1 >> idx2;
     idx1--;
     idx2--;
-    v[idx1].push_back(idx2);
+    grafo[idx1].push_back(idx2);
     lastIdx = idx1;
   }
 
-  dfs(v, words, lastIdx);
-  cout<<endl;
+  dfs(grafo, words, lastIdx);
+  cout << endl;
 
   return 0;
 }
